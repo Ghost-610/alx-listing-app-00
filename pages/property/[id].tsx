894238200e2d7ba31d874631fbf85622f6d459d0
elorm gershon <alidugershon@gmail.com>
2025-08-01 21:11:09 +0000

    repeat

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -2,6 +2,7 @@ import { PROPERTY_LISTING_SAMPLE } from "@/constants/index";
 import { useRouter } from "next/router";
 import PropertyDetail from "@/components/property/PropertyDetail";
 
+
 export default function PropertyPage() {
   const router = useRouter();
   const { id } = router.query;
